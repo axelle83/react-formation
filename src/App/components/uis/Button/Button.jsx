cb325941835d6b7f70a0096fc07feaf44c381b59
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-09 15:16:19 +0200

    réorganisation button

--- src/App/components/uis/Button/Button.jsx
+++ src/App/components/uis/Button/Button.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 function Button(props) {
     console.log(props);
-    return (<button className="Button" onClick={(evt) => {
-        console.log(evt);
-        props.onbuttonclick("coucou");
-    }} >{props.text}</button>);    
+    return (
+    <button 
+        className="Button" 
+        onClick={(evt) => {
+            // traitement de l'event préalable propre au composant
+            console.log(evt);
+            // déclenchement de le fonction envoyée par les props 
+            props.onbuttonclick("coucou");
+        }} 
+    >
+        {props.text}</button>);    
 }
 
 /*const Button = (props) => {
