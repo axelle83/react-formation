1f7f22cdffcfe655a840ab3e456d13948d46850f
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-09 14:50:58 +0200

    Add Button

---
+++ src/App/components/uis/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Button(props) {
+    return (<button className="Button">Benjamin</button>);    
+}
+
+/*const Button = (props) => {
+    return <button>Benjamin</button>
+}*/
+
+/*const Button = (props) => <button>Benjamin</button>;*/
+
+export default Button;
