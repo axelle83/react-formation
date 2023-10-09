978abe0166265748caee0a6c13e825572499a268
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-09 14:52:21 +0200

    Mise en eourev component button

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Button from './components/uis/Button/Button';
 
 function App() {
   return (
     <div className="App">
       Hello React
+      <hr/>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
     </div>
   );
 }
