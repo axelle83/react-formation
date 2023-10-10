6692eae8368834b3d1a4d617e213eaf80f804468
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-10 12:34:31 +0200

    Reorganisation folder + layout.jsx

--- src/App/components/layouts/FlexHGrow/FlexHGrox.jsx
+++ src/App/components/layouts/FlexHGrow/FlexHGrox.jsx
@@ -1,21 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FlexHGrox.module.css';
 
 const FlexHGrox = (props) => {
-  const [state, setstate] = useState({});
-  useEffect(() => {
-    return () => {
-    };
-  }, [])
   return (
     <div className={styles.FlexHGrox} data-testid="FlexHGrox">
-      FlexHGrox Component
+      {props.children}
     </div>
   );
 };
 
-FlexHGrox.propTypes = {};
+FlexHGrox.propTypes = {
+  children: PropTypes.any.isRequired,
+};
 
 FlexHGrox.defaultProps = {};
 
--- src/App/components/layouts/FlexWGrow/FlexWGrow.jsx
+++ src/App/components/layouts/FlexWGrow/FlexWGrow.jsx
@@ -1,21 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FlexWGrow.module.css';
 
 const FlexWGrow = (props) => {
-  const [state, setstate] = useState({});
-  useEffect(() => {
-    return () => {
-    };
-  }, [])
   return (
     <div className={styles.FlexWGrow} data-testid="FlexWGrow">
-      FlexWGrow Component
+      {props.children}
     </div>
   );
 };
 
-FlexWGrow.propTypes = {};
+FlexWGrow.propTypes = {
+  children: PropTypes.any.isRequired,
+};
 
 FlexWGrow.defaultProps = {};
 
