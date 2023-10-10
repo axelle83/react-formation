27faa35f27f988b6d58b09f35ed6f018d67dc813
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-10 15:39:14 +0200

    MemeSVGViewer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,36 +4,19 @@ import FlexHGrow from "./components/layouts/FlexHGrow/FlexHGrow";
 import FlexWGrow from "./components/layouts/FlexWGrow/FlexWGrow";
 import Header from "./components/uis/Header/Header";
 import Navbar from "./components/uis/Navbar/Navbar";
-import MemeSVGViewer from "./components/uis/MemeSVGViewer/MemeSVGViewer";
+import { MemeSVGViewer, emptyMeme } from "orsys-tjs-meme";
 import Footer from "./components/uis/Footer/Footer";
 import MemeForm from "./components/functionnals/MemeForm/MemeForm";
 
 
 function App() {
-  const [counter, setcounter] = useState(-1);
-  // componentDidMount
-  useEffect(() => {
-    //corps du cycle de vie
-    setcounter(0);
-    // démontage de composant
-    //return () => {
-      //effect
-      //
-    //};
-  }, [])
-  // componentDidUpdate -> mise à jour counter
-  useEffect(() => {
-    //corps du cycle de vie
-    console.log('didUpdate sur effect', counter);
-  }, [counter])
-  //const [userState, setUserState] = useState({name:"dd",value:0});
   return (
     <div className="App">
       <FlexHGrow>
         <Header/>
         <Navbar/>
         <FlexWGrow>
-          <MemeSVGViewer/>
+          <MemeSVGViewer basePath="" meme={emptyMeme}/>
           <MemeForm/>
         </FlexWGrow>
         <Footer/>
@@ -41,6 +24,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
