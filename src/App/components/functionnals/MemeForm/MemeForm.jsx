638de76bfc03908a3dac5cb56eeeb2da729d4315
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-10 15:40:55 +0200

    MemeForm

--- src/App/components/functionnals/MemeForm/MemeForm.jsx
+++ src/App/components/functionnals/MemeForm/MemeForm.jsx
@@ -1,22 +1,113 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import styles from './MemeForm.module.css';
+import React  from "react";
+import PropTypes from "prop-types";
+import styles from "./MemeForm.module.css";
+import Button from "../../uis/Button/Button";
 
 const MemeForm = (props) => {
-  const [state, setstate] = useState({});
-  useEffect(() => {
-    return () => {
-    };
-  }, [])
   return (
     <div className={styles.MemeForm} data-testid="MemeForm">
-      MemeForm Component
+      <form>
+        <label htmlFor="titre">
+          <h1>Titre</h1>
+        </label>
+        <br />
+        <input name="titre" id="titre" />
+        <hr />
+        <label htmlFor="image">
+          <h2>Image</h2>
+        </label>
+        <br />
+        <select name="image" id="image">
+          <option value="-1">Pas d'image</option>
+        </select>
+        <hr />
+        <label htmlFor="text">
+          <h2>texte</h2>
+        </label>
+        <br />
+        <input name="text" id="text" type="text" />
+        <br />
+        <label htmlFor="x">
+          <h2 className={styles.inline}>x :</h2>
+        </label>
+        <input className={styles.smallNumber} name="x" id="x" type="number" />
+        <label htmlFor="y">
+          <h2 className={styles.inline}>y :</h2>
+        </label>
+        <input className={styles.smallNumber} name="y" id="y" type="number" />
+        <hr />
+        <br />
+        <h2>Decorations</h2>
+        <label htmlFor="color">
+          <h2 className={styles.inline}>color :</h2>
+        </label>
+        <input name="color" id="color" type="color" />
+        <br />
+        <label htmlFor="fontSize">
+          <h2 className={styles.inline}>font-size :</h2>
+        </label>
+        <input
+          className={styles.smallNumber}
+          name="fontSize"
+          id="fontSize"
+          type="number"
+          min="0"
+        />
+        px
+        <br />
+        <label htmlFor="fontWeight">
+          <h2 className={styles.inline}>font-weight :</h2>
+        </label>
+        <input
+          className={styles.smallNumber}
+          name="fontWeight"
+          id="fontWeight"
+          type="number"
+          min="100"
+          step="100"
+          max="900"
+        />
+        <br />
+        <input name="underline" id="underline" type="checkbox" />
+        &nbsp;
+        <label htmlFor="underline">
+          <h2 className={styles.inline}>underline</h2>
+        </label>
+        &nbsp;<h2 className={styles.inline}>/</h2>&nbsp;
+        <label htmlFor="italic">
+          <h2 className={styles.inline}>italic</h2>
+        </label>
+        &nbsp;
+        <input name="italic" id="italic" type="checkbox" />
+        <hr />
+        <br />
+        <label htmlFor="frameSizeX">
+          <h2 className={styles.inline}>frame size X :</h2>
+        </label>
+        <input
+          className={styles.smallNumber}
+          name="frameSizeX"
+          id="frameSizeX"
+          type="number"
+          min="0"
+        />
+        px
+        <label htmlFor="frameSizeY">
+          <h2 className={styles.inline}>frame size y :</h2>
+        </label>
+        <input
+          className={styles.smallNumber}
+          name="frameSizeY"
+          id="frameSizeY"
+          type="number"
+          min="0"
+        />
+        px
+        <br />
+        <Button type={"submit"}>OK</Button>
+      </form>
     </div>
   );
 };
-
-MemeForm.propTypes = {};
-
-MemeForm.defaultProps = {};
 
 export default MemeForm;
