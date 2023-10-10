26b2b22410603b85a1bb9c77c7e6b62cda84e6be
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-10 10:00:37 +0200

    useState + useEffect

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/uis/Button/Button";
 
 function App() {
+  const [counter, setcounter] = useState(-1);
+  // componentDidMount
+  useEffect(() => {
+    //corps du cycle de vie
+    setcounter(0);
+    // démontage de composant
+    //return () => {
+      //effect
+      //
+    //};
+  }, [])
+  // componentDidUpdate -> mise à jour counter
+  useEffect(() => {
+    //corps du cycle de vie
+    console.log('didupdate sur effect', counter);
+  }, [counter])
+  //const [userState, setUserState] = useState({name:"dd",value:0});
   return (
     <div className="App">
-      Hello React
+      Valeur counter : {counter}
+      {/*Valeur counter de userstate : {userState.value}*/}
       <hr />
       <Button 
-        onbuttonclick={(param) => {
-          console.log("action", param);
+        onbuttonclick={() => {
+          setcounter(counter-1);
+          //setUserState({...userState,value:userState.value-1});
+          console.log(counter);
         }}
-        children="test"
-      />
-      <Button text="Annuler">
-        <img alt="img" src="https://cdn3.iconfinder.com/data/icons/miscellaneous-80/60/check-256.png" />
-        Coucou
+        >
+          -1
       </Button>
-      <Button text="Benjamin" type="coucou"/>
-      <Button type="button"><div>test</div></Button>
+      <Button 
+        text="Annuler"
+        onbuttonclick={()=> {
+          setcounter(counter+1);
+          console.log(counter);
+        }}
+      >
+        +1
+      </Button>      
     </div>
   );
 }
