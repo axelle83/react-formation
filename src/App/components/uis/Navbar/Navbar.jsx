a39e6b4afaf93ba0a2cb1b7599b2ddaf24a8f058
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-10 14:49:55 +0200

    Navbar: import from bootstrap

--- src/App/components/uis/Navbar/Navbar.jsx
+++ src/App/components/uis/Navbar/Navbar.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Navbar.module.css';
+import { Nav, Container, Navbar as NavBar } from 'react-bootstrap';
+import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const Navbar = (props) => {
-  const [state, setstate] = useState({});
-  useEffect(() => {
-    return () => {
-    };
-  }, [])
   return (
     <div className={styles.Navbar} data-testid="Navbar">
-      Navbar Component
+      <NavBar bg="dark" data-bs-theme="dark">
+        <Container>
+          <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </NavBar>
     </div>
   );
 };
-
-Navbar.propTypes = {};
-
-Navbar.defaultProps = {};
 
 export default Navbar;
