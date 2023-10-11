600b4fe2713fa485d0ded768320bcd6933cdd8c6
axelle83 <axelle.lecroq@yahoo.fr>
2023-10-11 10:03:31 +0200

    rxslice

---
+++ src/App/store/currentSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const currentSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = currentSlice.actions
+
+export default currentSlice.reducer
